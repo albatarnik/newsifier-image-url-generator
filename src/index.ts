810dc2fb45d2f2d15afccca92ed9f144b37d030f
Kamal <kamalalbatarny@gmail.com>
2022-10-26 13:05:09 -0300

    Change the types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Resizer } from "./resizer";
-export * from './resizer';
+export * from "./resizer";
 
-export function renderImage(image_url : String,config :any,render_options :any )
-{
-    let resizer = new Resizer(config.service,config.host)
+export function renderImage(
+  image_url: string,
+  config: any,
+  render_options: any
+) {
+  let resizer = new Resizer(config.service, config.host);
 
-    return resizer.resize(image_url,render_options);
+  return resizer.resize(image_url, render_options);
 }
--- src/resizer.ts
+++ src/resizer.ts
@@ -2,34 +2,41 @@ import { BasePrcessor } from "./base-prcessor";
 import { Imagix } from "./imagix";
 import { ImgProxy } from "./img-proxy";
 
-export class Resizer
-{
-    protected service:string;
-  
-    protected service_host:string;
-    /**
-     * 
-     * @param service 
-     * @param service_host 
-     */
-    constructor(service:string, service_host:string )
-    {
-        this.service_host = service_host
-        this.service = service
-    }
+export class Resizer {
+  protected service: string;
 
-    resize(image_url : String ,render_options :any)
-    {
-        if( !render_options ) render_options = {}
-        let processor = this.getProcessor(image_url ,{service:this.service,host:this.service_host} , render_options )
-        return processor.getParsedUrl();
-    }
+  protected service_host: string;
+  /**
+   *
+   * @param service
+   * @param service_host
+   */
+  constructor(service: string, service_host: string) {
+    this.service_host = service_host;
+    this.service = service;
+  }
 
-    getProcessor(image_url : String,config :any,render_options :any )
-    {
-        let processor = new ImgProxy(image_url,config.host,render_options);
-        if(config.service === BasePrcessor.IMGIX )
-            processor = new Imagix(image_url,config.host,render_options);
-        return processor;
-    }
+  resize(image_url: string, render_options: any) {
+    
+
+    // check if the image is already resized
+    if (image_url.includes(this.service_host)) return image_url;
+
+    
+    if (!render_options) render_options = {};
+    let processor = this.getProcessor(
+      image_url,
+      { service: this.service, host: this.service_host },
+      render_options
+    );
+
+    return processor.getParsedUrl();
+  }
+
+  getProcessor(image_url: string, config: any, render_options: any) {
+    let processor = new ImgProxy(image_url, config.host, render_options);
+    if (config.service === BasePrcessor.IMGIX)
+      processor = new Imagix(image_url, config.host, render_options);
+    return processor;
+  }
 }
