9f6241c36d735b8b8908367e9c4c62652b8d24c1
Kamal <kamalalbatarny@gmail.com>
2022-12-01 14:17:52 -0300

    add webp

--- src/img-proxy.ts
+++ src/img-proxy.ts
@@ -1,58 +1,51 @@
 import { BasePrcessor } from "./base-prcessor";
 
-export class ImgProxy extends BasePrcessor
-{
+export class ImgProxy extends BasePrcessor {
+  prepareUrl() {
+    let template =
+      "{domain}/{sign}/resizing_type:fill/{watermark}/{width}/{height}/plain/{url}";
+    template = template.replace("{domain}", this.host);
+    template = template.replace("{sign}", "Acbs8526SDKI");
+    template = template.replace("{url}", this.url);
 
-    prepareUrl()
-    {
-        let template = "{domain}/{sign}/resizing_type:fill/{watermark}/{width}/{height}/plain/{url}";
-        template = template.replace("{domain}",this.host);
-        template = template.replace("{sign}","Acbs8526SDKI");
-        template = template.replace("{url}",this.url);
+    this.url = template;
+  }
 
-        this.url = template
+  checkReadyUrl(): void {
+    if (!this.url.endsWith("@webp")) {
+      this.url = this.url + "@webp";
     }
-    
+  }
 
-    prepareOptions()
-    {
-        if( BasePrcessor.AR in this.render_options )
-        {
-            if( BasePrcessor.W in this.render_options )
-            {
-                let ar = this.render_options[BasePrcessor.AR].split(':');
-                if( ar && ar.length == 2 )
-                {
-                    let h = (ar[1]/ar[0]) * this.render_options[BasePrcessor.W];
-                    if( h ) 
-                    {
-                        h = Math.round(h);
-                        this.render_options[BasePrcessor.H] = h;
-                    }
-                }
-            }
+  prepareOptions() {
+    if (BasePrcessor.AR in this.render_options) {
+      if (BasePrcessor.W in this.render_options) {
+        let ar = this.render_options[BasePrcessor.AR].split(":");
+        if (ar && ar.length == 2) {
+          let h = (ar[1] / ar[0]) * this.render_options[BasePrcessor.W];
+          if (h) {
+            h = Math.round(h);
+            this.render_options[BasePrcessor.H] = h;
+          }
         }
+      }
     }
+  }
 
-    width(val)
-    {
-        let to = val ? "/width:"+val: "";
- 
-        this.url = this.url.toString().replace("/{width}",to);
-    }
+  width(val) {
+    let to = val ? "/width:" + val : "";
 
-    height(val)
-    {
-        let to = val ? "/height:"+val: "";
-    
-        this.url = this.url.replace("/{height}",to);
-    }
-    watermark(val)
-    {
-        if( val )
-            val = encodeURIComponent(val)
-        let to = val ? "/watermark:"+val: "";
-        this.url = this.url.replace("/{watermark}",to);
-    }
+    this.url = this.url.toString().replace("/{width}", to);
+  }
 
+  height(val) {
+    let to = val ? "/height:" + val : "";
+
+    this.url = this.url.replace("/{height}", to);
+  }
+  watermark(val) {
+    if (val) val = encodeURIComponent(val);
+    let to = val ? "/watermark:" + val : "";
+    this.url = this.url.replace("/{watermark}", to);
+  }
 }
